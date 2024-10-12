c7a5d467a29425180c909c46b92e6efdc9b62d63
AnonymousPC <160385726+AnonymousPC@users.noreply.github.com>
2024-10-12 14:51:58 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#define homework
+#include "../generic/basic/interface.h"
+#include "../generic/container/interface.h"
+//#include "../generic/io/interface.h"
+#include "../generic/math/interface.h"
+//#include "../specific/neural/interface.h"
+#include "../specific/spirit/interface.h"
+//#include "../specific/stock/interface.h"
+
+using namespace ap;
+
+int main ( )
+{
+    spirit::cmd();
+}
+
